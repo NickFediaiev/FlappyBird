ca7639e4381de424cd00febd6620dfe20469ab26
Nick Fediaiev <Nick.Fediaiev@gmail.com>
2021-01-05 12:31:14 +0200

    Set up Bird and Ground in scene

---
+++ Assets/Scripts/BirdFly.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdFly : MonoBehaviour
+{
+    public float Velocity = 1f;
+
+    [SerializeField] private Rigidbody2D _rigidbody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //jump
+            _rigidbody.velocity = Vector2.up * Velocity;
+        }
+    }
+}
