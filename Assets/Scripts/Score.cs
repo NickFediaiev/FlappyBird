159c84d25ee70f3cbf9e0f76148e2fbe8acc5a52
Nick Fediaiev <Nick.Fediaiev@gmail.com>
2021-01-05 14:13:48 +0200

    Implemented Scoring

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private Text _scoreText;
+
+    public static int score = 0;
+    
+    void Start()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        _scoreText.text = score.ToString();
+    }
+}
